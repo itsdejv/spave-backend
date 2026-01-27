086c186e11733b1042046fbe57a9df92c87a15cd
David Rihacek <davidrihacek@David--MacBook-Pro.local>
2026-01-27 21:46:11 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is currently running on http://localhost:${PORT}`);
+});
