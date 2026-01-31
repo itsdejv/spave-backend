3f12bfb3611f0498d8a65b74166cf1a42edfa83b
David Rihacek <itssdejv@gmail.com>
2026-02-01 00:41:57 +0100

    prepared for generation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../build/swagger.json";
 import { RegisterRoutes } from "./generated/routes";
 import { errorHandler } from "./middlewares/error-handler";
 
@@ -11,6 +12,10 @@ app.use("/docs", swaggerUi.serve, async (_req: any, res: any) => {
   return res.send(
     swaggerUi.generateHTML(await import("../build/swagger.json")),
   );
+});
+
+app.get("/swagger-json", (_req, res) => {
+  res.json(swaggerDocument);
 });
 
 RegisterRoutes(app);
